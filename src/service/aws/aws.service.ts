e210b4951d2f622a21e0c941caefa8292c9c2923
zavich <vinicius.halter@gmail.com>
2026-04-04 01:19:42 -0300

    fix: update S3 client region environment variable to AWS_S3_REGION

--- src/service/aws/aws.service.ts
+++ src/service/aws/aws.service.ts
@@ -117,7 +117,7 @@ export class AwsServices {
     return file;
   }
   private s3Client = new S3Client({
-    region: process.env.AWS_REGION as string,
+    region: process.env.AWS_S3_REGION as string,
     credentials: {
       accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
       secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
