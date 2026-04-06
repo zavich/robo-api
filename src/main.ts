dec9299217def649c3c3ca78f6d495229bc7039b
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-06 13:48:10 -0300

    fix: corrigir URL de origem CORS removendo duplicação

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:8081',
       'http://localhost:3000',
-      'http://http://217.196.61.185:8081',
+      'http://217.196.61.185:8081',
       'https://scraping-robo-production.up.railway.app',
       'https://scraping-robo-staging.up.railway.app',
       'https://painel-robo-production.up.railway.app',
