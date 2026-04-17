1cf8d21d2f911bb81696927931e234e4d3ea80b2
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-17 18:02:01 -0300

    feat: remover credenciais do S3 do código e manter apenas a configuração da região

--- src/service/aws/aws.service.ts
+++ src/service/aws/aws.service.ts
@@ -126,9 +126,5 @@ export class AwsServices {
   }
   private s3Client = new S3Client({
     region: process.env.AWS_S3_REGION as string,
-    credentials: {
-      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
-    },
   });
 }
